9fb126ba52a6d6e4905e55159593660d5cd4257d
dannylittle715 <63871655+dannylittle715@users.noreply.github.com>
2021-01-06 19:37:03 -0500

    Update 404.js
    changed text slightly

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ const Error = () => {
     <Layout page='Projects'>
       <h1>Uh Oh!</h1>
       <p>
-        You visited a page that doesn't exist on my website. Sorry about that.
+        You visited a page that doesn't exist.
         Feel free to{' '}
         <Link href='/'>
           <a>Go Home</a>
