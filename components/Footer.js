bdc81ab61c12a75082e3f29236b7c7d27a8a5568
Danny Little <dannyl9979@gmail.com>
2021-01-13 16:06:38 -0500

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,12 @@ const DesktopHeader = () => {
   return (
     <StyledFooter>
       <div className='inner-container'>
-        <p className='header-row'>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
           <Link href='https://github.com/dannylittle715'>
             <a>Github</a>
           </Link>
@@ -19,8 +24,8 @@ const DesktopHeader = () => {
           <Link href='https://www.instagram.com/dlittle_music/'>
             <a>Instagram</a>
           </Link>
-        </p>
-        <p className='header-row'>Made by Danny Little in 2020.</p>
+        </div>
+        <div style={{ textAlign: 'center' }}>Made by Danny Little in 2020.</div>
       </div>
     </StyledFooter>
   );
@@ -35,19 +40,16 @@ const StyledFooter = styled.footer`
   bottom: 0;
   width: 100%;
   display: grid;
-  grid-template-columns: max(300px, 20%);
-  align-items: center;
+  grid-template-columns: 300px;
   justify-content: center;
   color: white;
   flex-shrink: 0;
+  height: 150px;
 
   .inner-container {
-    display: flex;
-    flex-direction: column;
-    padding: 3rem 0 2rem;
-  }
-  .header-row {
-    display: flex;
-    justify-content: space-around;
+    display: grid;
+    grid-template-rows: auto auto;
+    gap: 15px;
+    align-content: center;
   }
 `;
