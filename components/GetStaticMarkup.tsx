0f253283abc592de817168d62f3845dc34115e0c
Danny Little <dannyl9979@gmail.com>
2023-03-08 23:45:01 -0500

    example of issue

--- components/GetStaticMarkup.tsx
+++ components/GetStaticMarkup.tsx
@@ -1,34 +1,13 @@
 import Mdx from "./Mdx";
 import { getSourceBySlug } from "@/lib/mdx";
-import CoolComponent from "./CoolComponent";
 import { MDXRemote } from "next-mdx-remote/rsc";
-
-export default function None() {
-  return;
-}
 
 export async function generateStaticMarkup(slug: string) {
   const ReactDomServer = (await import("react-dom/server")).default;
 
-  let element: JSX.Element;
-  // element = <CoolComponent>hello</CoolComponent>;
   const mdxSource = getSourceBySlug(slug);
-  // element = await (<Mdx source={mdxSource} />);
-  element = await (<MDXRemote source={mdxSource} compiledSource="" />);
-
-  // uncommenting the lines below makes the rss feed generate correctly
-
-  // element = (
-  //   <div>
-  //     <a
-  //       href={`${relative_url}/blog/${slug}`}
-  //     >{`${relative_url}/blog/${slug}`}</a>
-  //   </div>
-  // );
-
-  console.log(element);
+  // const element = await (<Mdx source={mdxSource} />);
+  const element = await (<MDXRemote source={`#hi`} compiledSource="" />);
   const staticMarkup = ReactDomServer.renderToStaticMarkup(element);
-  console.log(staticMarkup);
-
   return staticMarkup;
 }
